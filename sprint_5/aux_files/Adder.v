0e54da9a5292e4c954470a4aa1e145a1ea2d80cd
Vitorluca <vitor8391244603@gmail.com>
2024-03-19 12:14:47 -0300

    add file adder and modification control  unit

--- sprint_5/aux_files/Adder.v
+++ sprint_5/aux_files/Adder.v
@@ -9,6 +9,7 @@ output reg [7:0] out_adder);
 
 CONST = 3'h4 //adiciona 4 ao indereco levando para a proxima instrucao
 always@(*)
-	out_adder = 
+	out_adder = PC + CONST;
+	PC = out_adder; //atualiza o PC para a proxima interacao
 	
 endmodule
