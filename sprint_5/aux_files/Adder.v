740638e4f75b5241a8f6f630217f2df28f50d321
Vitorluca <vitor8391244603@gmail.com>
2024-03-19 12:13:47 -0300

    add file adder and modification control unit

--- sprint_5/aux_files/Adder.v
+++ sprint_5/aux_files/Adder.v
@@ -3,14 +3,12 @@ module ADDER_4 (
 //input data
 input [7:0] PC,
 input [2:0] CONST,
-input sel,
 
 //output signal
-output reg [BITS-1:0] out_mux);
-	always@(*)
-		if(sel == 0)
-			out_mux= i0;
-		else
-			out_mux = i1;
+output reg [7:0] out_adder);
+
+CONST = 3'h4 //adiciona 4 ao indereco levando para a proxima instrucao
+always@(*)
+	out_adder = 
 	
 endmodule
